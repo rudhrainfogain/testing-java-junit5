c70d2240d77c9c10f09b95fa3ea1e19e3408be93
rudhra koul <techpertitsolutions@gmail.com>
2019-03-14 02:16:59 +0530

    Dependent Assertions

---
+++ src/test/java/com/infogain/petclinic/model/OwnerTest.java
@@ -0,0 +1,24 @@
+package com.infogain.petclinic.model;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerTest {
+
+    @Test
+    void dependentAssertions() {
+
+        Owner owner = new Owner(1l, "Rudhra", "Koul");
+        owner.setCity("Greater Noida");
+        owner.setTelephone("1231231234");
+
+        assertAll("Properties Test",
+                () -> assertAll("Person Properties",
+                        () -> assertEquals("Rudhra", owner.getFirstName(), "First Name Did not Match"),
+                        () -> assertEquals("Koul", owner.getLastName())),
+                () -> assertAll("Owner Properties",
+                        () -> assertEquals("Greater Noida", owner.getCity(), "City Did Not Match"),
+                        () -> assertEquals("1231231234", owner.getTelephone())
+                ));
+    }
+}
