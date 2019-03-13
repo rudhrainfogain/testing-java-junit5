605d517e9d0aedb85db2d5bd6196d0537aff4261
rudhra koul <techpertitsolutions@gmail.com>
2019-03-14 02:11:07 +0530

    Grouped assertions

---
+++ src/test/java/com/infogain/petclinic/model/PersonTest.java
@@ -0,0 +1,29 @@
+package com.infogain.petclinic.model;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    @Test
+    void groupedAssertions() {
+        //given
+        Person person = new Person(1l, "Rudhra", "Koul");
+
+        //then
+        assertAll("Test Props Set",
+                () -> assertEquals(person.getFirstName(), "Rudhra"),
+                () -> assertEquals(person.getLastName(), "Koul"));
+    }
+
+    @Test
+    void groupedAssertionMsgs() {
+        //given
+        Person person = new Person(1l, "Rudhra", "Koul");
+
+        //then
+        assertAll("Test Props Set",
+                () -> assertEquals(person.getFirstName(), "Rudhra", "First Name Failed"),
+                () -> assertEquals(person.getLastName(), "Koul", "Last Name Failed"));
+    }
+}
