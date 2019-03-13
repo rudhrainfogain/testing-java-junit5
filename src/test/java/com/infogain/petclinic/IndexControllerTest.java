d4199100ae4abb736a9108d3d195cfeaeca7dbd1
rudhra koul <techpertitsolutions@gmail.com>
2019-03-14 02:26:26 +0530

    Display Name Demo

--- src/test/java/com/infogain/petclinic/IndexControllerTest.java
+++ src/test/java/com/infogain/petclinic/IndexControllerTest.java
@@ -3,6 +3,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import com.infogain.petclinic.controllers.IndexController;
@@ -16,6 +17,7 @@ class IndexControllerTest {
         controller = new IndexController();
     }
 
+    @DisplayName("Test Proper View name is returned for index page")
     @Test
     void index() {
         assertEquals("index", controller.index());
@@ -26,6 +28,7 @@ class IndexControllerTest {
     }
 
     @Test
+    @DisplayName("Test exception")
     void oupsHandler() {
         assertTrue("notimplemented".equals(controller.oupsHandler()), () -> "This is some expensive " +
                 "Message to build" +
